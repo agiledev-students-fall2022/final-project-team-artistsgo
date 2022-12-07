7e8dd19b7d951c0f77c84f45f0099e85c519e004
Derek Han <82979043+Alograti@users.noreply.github.com>
2022-12-07 15:59:51 -0500

    Updating in efforts to find missing module
    Deploying on droplet is saying we're missing the module. Trying different approach to requiring as per Vanessa's instructions.

--- back-end/Server.js
+++ back-end/Server.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
-const server = require("./app") // load up the web server
+const server = require(require('path').resolve(__dirname, 'app.js'))
+// const server = require("./app") // load up the web server
 const port = 3001 // the port to listen to for incoming requests
 // call express's listen function to start listening to the port
 
