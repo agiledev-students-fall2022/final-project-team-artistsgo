2e6ce9811948d4f4cfdbda0b19887bf6f02057ed
Derek Han <82979043+Alograti@users.noreply.github.com>
2022-12-07 11:54:19 -0500

    Update Server.js

--- back-end/Server.js
+++ back-end/Server.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-const server = require("./App") // load up the web server
+const server = require("./app") // load up the web server
 const port = 3001 // the port to listen to for incoming requests
 // call express's listen function to start listening to the port
 
