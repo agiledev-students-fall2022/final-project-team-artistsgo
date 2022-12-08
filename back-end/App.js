a56502940c2a14cc762c272840d599ff4467bfd4
FairyyGenie <ch3801@nyu.edu>
2022-12-07 23:59:22 -0500

    Merge remote-tracking

--- back-end/App.js
+++ back-end/App.js
@@ -32,7 +32,7 @@ app.use("/static", express.static("public"))
 
 app.use(
   cors({
-    origin: "http://localhost:3001",
+    origin: "http://localhost:3000",
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
   })
